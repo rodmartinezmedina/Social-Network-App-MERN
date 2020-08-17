aa12fda3ca1c1c9855421a233ba4b483731f1b67
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-08-17 22:17:53 +0200

    post model created

---
+++ models/PostsModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  //name of the user, not the post!!
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: string,
+  },
+  //An array of the users who liked the post.
+  //To avoid a user to like the post multiple times
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: string,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
