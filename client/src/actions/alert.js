58f69aa5b0df1a2879f46d8a41606e4b712d3257
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-08-20 14:36:13 +0200

    finished alerts now do auth

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 //For beeing able to dispatch more than one action from this function
 //we must use thunk
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 3000) => (dispatch) => {
   const id = uuidv4();
   dispatch({
     type: SET_ALERT,
@@ -14,5 +14,5 @@ export const setAlert = (msg, alertType) => (dispatch) => {
     },
   });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 3000);
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
