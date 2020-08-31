aa5e899a934301e52fdb2811d56e73f9fdf58d40
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-08-31 22:22:05 +0200

    single post page works

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getPost, addLike, removeLike, deletePost } from "../../actions/post";
+import PostItem from "../posts/PostItem";
+
+//fpor getting the id from the URL we pull out props.match
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, {
+  getPost,
+})(Post);
