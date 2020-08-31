4d65a6981a986594f7e3a50348a520db406d703a
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-08-31 16:17:17 +0200

    display posts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -15,3 +15,5 @@ export const NO_REPOS = "NO_REPOS";
 export const PROFILE_ERROR = "PROFILE_ERROR";
 export const CLEAR_PROFILE = "CLEAR_PROFILE";
 export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
+export const GET_POSTS = "GET_POSTS";
+export const POST_ERROR = "POST_ERROR";
