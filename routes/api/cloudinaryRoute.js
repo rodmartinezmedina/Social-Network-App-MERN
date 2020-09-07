c17d313331115f95de7b473fe399e9a75106e3cc
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-09-07 15:28:49 +0200

    I CAN ADD PROFILE PHOTO!!!!!!!!! F..., CLOUDINARY

--- routes/api/cloudinaryRoute.js
+++ routes/api/cloudinaryRoute.js
@@ -7,18 +7,14 @@ const parser = require("../../config/cloudinary");
 
 // The base Cloudinary API endpoint looks like this:
 // https://api.Cloudinary.com/v1_1/:cloud_name/:action
+// "https://api.Cloudinary.com/v1_1/:dvioc75zu/image/upload",
+router.post("/", parser.single("userImg"), (req, res, next) => {
+  console.log("inside cloudinary upload route");
 
-router.post(
-  "https://api.Cloudinary.com/v1_1/:dvioc75zu/image/upload",
-  parser.single("userImg"),
-  (req, res, next) => {
-    console.log("inside cloudinary upload route");
-
-    let userImg = {};
-    const image_url = req.file.secure_url;
-    res.status(201).json(image_url);
-    console.log(userImg.url);
-  }
-);
+  let userImg = {};
+  const image_url = req.file.secure_url;
+  res.status(201).json(image_url);
+  // console.log(userImg.url);
+});
 
 module.exports = router;
