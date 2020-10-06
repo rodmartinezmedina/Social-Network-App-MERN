ba7618cab9ba8a504e810c8b0fee887432267895
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-10-06 23:53:28 +0200

    working on progile page

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -14,47 +14,65 @@ const ProfileTop = ({
   },
 }) => {
   return (
-    <div className="profile-top bg-primary p-2">
-      <img className="round-img my-1" src={userImg ? userImg : avatar} alt="" />
-      <h1 className="large">{name}</h1>
-      <p className="lead">
-        {status} {company && <span> at {company}</span>}
-      </p>
-      <p>{location && <span>{location}</span>}</p>
-      <div className="icons my-1">
-        {website && (
-          <a href={website} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-globe fa-2x"></i>
-          </a>
-        )}
-        {social && social.twitter && (
-          <a href={social.twitter} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-twitter fa-2x"></i>
-          </a>
-        )}
-        {social && social.facebook && (
-          <a href={social.facebook} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook fa-2x"></i>
-          </a>
-        )}
+    <div className="profile-top">
+      {/* 1 */}
+      <div className="profile-top__upper-container">
+        <img
+          className="round-img my-1"
+          src={userImg ? userImg : avatar}
+          alt=""
+        />
+        <h1 className="large">{name}</h1>
+      </div>
+      {/* 2 */}
+      <div className="profile-top__lower-container">
+        <p className="lead profile-top__status profile-name">
+          {status} {company && <span> at {company}</span>}
+        </p>
+        <p className="profile-top__location">
+          {" "}
+          {location && <span>{location}</span>}
+        </p>
 
-        {social && social.linkedin && (
-          <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-linkedin fa-2x"></i>
-          </a>
-        )}
+        <div className="icons bg-primary">
+          {website && (
+            <a href={website} target="_blank" rel="noopener noreferrer">
+              <i className="fas fa-globe fa-2x"></i>
+            </a>
+          )}
+          {social && social.twitter && (
+            <a href={social.twitter} target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter fa-2x"></i>
+            </a>
+          )}
+          {social && social.facebook && (
+            <a href={social.facebook} target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook fa-2x"></i>
+            </a>
+          )}
 
-        {social && social.youtube && (
-          <a href={social.youtube} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-youtube fa-2x"></i>
-          </a>
-        )}
+          {social && social.linkedin && (
+            <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin fa-2x"></i>
+            </a>
+          )}
 
-        {social && social.instagram && (
-          <a href={social.instagram} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-instagram fa-2x"></i>
-          </a>
-        )}
+          {social && social.youtube && (
+            <a href={social.youtube} target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-youtube fa-2x"></i>
+            </a>
+          )}
+
+          {social && social.instagram && (
+            <a
+              href={social.instagram}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-instagram fa-2x"></i>
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
