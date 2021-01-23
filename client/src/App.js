de1ede888cd773f79c9dd4fe50fdcddef4e7e232
John Doe <rodmartinezmedina@gmail.com>
2021-01-23 19:20:05 +0100

    profile single page now private route added sign in for seeing more profiles on profiles page

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,37 @@
-import React, { Fragment, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import NavbarAlternative from "./components/layout/NavbarAlternative";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alert from "./components/layout/Alert";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-form/CreateProfile";
-import EditProfile from "./components/profile-form/EditProfile";
-import AddExperience from "./components/profile-form/AddExperience";
-import AddEducation from "./components/profile-form/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
-import PrivateRoute from "./components/routing/PrivateRoute";
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import NavbarAlternative from './components/layout/NavbarAlternative'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/profile-form/CreateProfile'
+import EditProfile from './components/profile-form/EditProfile'
+import AddExperience from './components/profile-form/AddExperience'
+import AddEducation from './components/profile-form/AddEducation'
+import Profiles from './components/profiles/Profiles'
+import Profile from './components/profile/Profile'
+import Posts from './components/posts/Posts'
+import Post from './components/post/Post'
+import PrivateRoute from './components/routing/PrivateRoute'
 //
 //Redux
-import { Provider } from "react-redux";
-import store from "./store";
-import { loadUser } from "./actions/auth";
-import setAuthToken from "./utils/setAuthToken";
-import "./App.css";
+import { Provider } from 'react-redux'
+import store from './store'
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
+import './App.css'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token)
 }
 
 const App = () => {
   useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+    store.dispatch(loadUser())
+  }, [])
   //Hooks official Docs
   // Remember the [] tells React the effect doesn't depend on any values from props or state.
   // so it never needs to re run.
@@ -41,43 +41,43 @@ const App = () => {
       <Router>
         <Fragment>
           <NavbarAlternative />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
             <Alert />
             <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+              <PrivateRoute exact path='/profile/:id' component={Profile} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
-                path="/create-profile"
+                path='/create-profile'
                 component={CreateProfile}
               />
               <PrivateRoute
                 exact
-                path="/edit-profile"
+                path='/edit-profile'
                 component={EditProfile}
               />
               <PrivateRoute
                 exact
-                path="/add-experience"
+                path='/add-experience'
                 component={AddExperience}
               />
               <PrivateRoute
                 exact
-                path="/add-education"
+                path='/add-education'
                 component={AddEducation}
               />
-              <PrivateRoute exact path="/posts" component={Posts} />
-              <PrivateRoute exact path="/posts/:id" component={Post} />
+              <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
       </Router>
     </Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
