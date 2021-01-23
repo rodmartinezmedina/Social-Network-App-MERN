a4f27d545f229e9476656ae73427c38e538735f3
John Doe <rodmartinezmedina@gmail.com>
2021-01-23 23:11:08 +0100

    refactoring styles after cleanup

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,42 +1,44 @@
-import React from "react";
-import { Link, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
-    return <Redirect to="/dashboard" />;
+    return <Redirect to='/dashboard' />
   }
 
   return (
-    <div className="landing">
-      <h1 className="x-large" id="nav-logo"><i className="fas fa-code-branch"></i> TechNetwork</h1>
-      
-      <h2 className="medium">
+    <div className='landing'>
+      <h1 className='x-large' id='nav-logo'>
+        <i className='fas fa-code-branch'></i> TechNetwork
+      </h1>
+
+      <h2 className='font2-reg medium'>
         Developers, Data Scientists, Engineers and more...
       </h2>
-      <p className="lead">
+      <p className='font2-light lead'>
         Create a profile/portfolio, share posts, network and get help from other
         peers.
       </p>
-      <div className="buttons">
-        <Link to="/register" className="btn btn-primary">
+      <div className='buttons'>
+        <Link to='/register' className='btn btn-primary'>
           Sign Up
         </Link>
-        <Link to="/login" className="btn btn-light">
+        <Link to='/login' className='btn btn-light'>
           Login
         </Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
 Landing.propTypes = {
   isAuthenticated: PropTypes.bool,
-};
+}
 
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
-});
+})
 
-export default connect(mapStateToProps)(Landing);
+export default connect(mapStateToProps)(Landing)
